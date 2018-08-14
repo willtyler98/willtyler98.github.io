899c70f9a3d496ca12942a5f00bc6de0b1aebbdf
Will Tyler <will.tyler11@gmail.com>
2018-08-14 15:39:29 -0400

    Setup app to use angular material, and generate side navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { ParticlesComponent } from './particles/particles.component';
 import { LinksComponent } from './links/links.component';
+import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ParticlesComponent,
-    LinksComponent
+    LinksComponent,
+    SideNavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
