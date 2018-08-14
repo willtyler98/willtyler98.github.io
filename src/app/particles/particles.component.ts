2556c0266fd079f550afc327addbf80919c2fadc
Will Tyler <will.tyler11@gmail.com>
2018-08-14 09:31:25 -0400

    Add particles component, particles.json

---
+++ src/app/particles/particles.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-particles',
+  templateUrl: './particles.component.html',
+  styleUrls: ['./particles.component.css']
+})
+export class ParticlesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
